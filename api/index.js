659d8e75dd8d228ee5614cdee97bed63028f43a1
leinerdev <leinerbarrios99.ljbm@gmail.com>
2023-04-02 12:42:26 -0500

    Move project to api folder to deploy in vercel

--- api/index.js
+++ api/index.js
@@ -5,19 +5,19 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send(`
     Esta es una API desarrollada en Nodejs con Express, si quieres ver los productos navega a la ruta: /api/v1/products.
     Toda la documentación la encuentras en: "https://documenter.getpostman.com/view/20530102/2s93RWMq17"
   `);
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
