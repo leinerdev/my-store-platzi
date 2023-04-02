05c5b1775d6ee005962e4ec2d59c8302ab3399d5
leinerdev <leinerbarrios99.ljbm@gmail.com>
2023-04-02 12:34:20 -0500

    New description

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
+  res.send(`
+    Esta es una API desarrollada en Nodejs con Express, si quieres ver los productos navega a la ruta: /api/v1/products.
+    Toda la documentación la encuentras en: "https://documenter.getpostman.com/view/20530102/2s93RWMq17"
+  `);
 });
 
 app.get('/nueva-ruta', (req, res) => {
